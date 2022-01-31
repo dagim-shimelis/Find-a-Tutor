ef901e39d8ae0e3eb188611afb490a1ac72a7798
Dagim Shimelis <79264107+dagim-shimelis@users.noreply.github.com>
2022-01-31 05:38:49 -0800

    clean slate
    this is clean code ready for the initialization of the development stage

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Jobs from '../views/Jobs/Jobs.vue'
-import JobDetail from '../views/Jobs/JobDetails.vue'
-import NoResult from '../views/NoResult.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/jobs',
-    name: 'Jobs',
-    component: Jobs
-  },
-  {
-    path: '/jobs/:id',
-    name: 'JobDetail',
-    component: JobDetail,
-    props:true
-  },
-  //404 error handler
-  {
-    path:'/:catchAll(.*)',
-    name:'NoResult',
-    component: NoResult
-  }
-
+ 
 ]
 
 const router = createRouter({
