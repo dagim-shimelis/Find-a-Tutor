5502f52a19af504cbb7f2549559a9304edac4808
Dagim Shimelis <79264107+dagim-shimelis@users.noreply.github.com>
2022-01-31 10:46:01 -0800

    added list
    created a list of coaches with demo data

---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,34 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'daniel',
+          lastName: 'beyene',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm daniel and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30
+        },
+        {
+          id: 'c2',
+          firstName: 'samri',
+          lastName: 'deneke',
+          areas: ['frontend', 'career'],
+          description:
+            'I am samri and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30
+        }
+      ]
+    };
+  },
+  mutations,
+  actions,
+  getters
+};
