5ab738b2d6844514155c5766dd2192443ed69886
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2022-02-01 16:48:13 +0300

    Fixed some typos

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -9,20 +9,20 @@ export default {
       coaches: [
         {
           id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
+          firstName: 'Abate',
+          lastName: 'Zergaw',
           areas: ['frontend', 'backend', 'career'],
           description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+            "I'm Abate and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
           hourlyRate: 30
         },
         {
           id: 'c2',
-          firstName: 'Julie',
-          lastName: 'Jones',
+          firstName: 'Mekdes',
+          lastName: 'Mustefa',
           areas: ['frontend', 'career'],
           description:
-            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+            'I am Mekdes and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
           hourlyRate: 30
         }
       ]
